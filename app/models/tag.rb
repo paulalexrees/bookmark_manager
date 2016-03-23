21091bb82e1b7837a522ba95836186e5222828bb
Paul <paulalexrees@gmail.com>
2016-03-23 13:45:24 +0000

    fixed DataMapper setup

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -10,7 +10,3 @@ class Tag
   has n, :links, :through => Resource
 
 end
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-# DataMapper.finalize
-# DataMapper.auto_upgrade!
