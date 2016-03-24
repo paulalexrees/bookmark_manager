d4bf81011a257d301bc4c37f3975578bf1c85d16
Paul <paulalexrees@gmail.com>
2016-03-24 10:50:18 +0000

    start of day four

--- app/models/data_mapper_setup.rb
+++ app/models/data_mapper_setup.rb
@@ -1,5 +1,5 @@
 require_relative 'link'
 require_relative 'tag'
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-# DataMapper.finalize
+DataMapper.finalize
 # DataMapper.auto_upgrade!
